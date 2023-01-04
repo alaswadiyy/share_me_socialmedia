68f158cb670ae1b3ee5cc3aab901db314bd8211f
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-01-04 21:39:58 +0100

    google login

--- shareme_frontend/src/components/Login.jsx
+++ shareme_frontend/src/components/Login.jsx
@@ -3,8 +3,13 @@ import { useNavigate } from 'react-router-dom';
 import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logo.png";
+import GoogleLogin from 'react-google-login';
 
 const Login = () => {
+
+  const responseGoogle= (response) => {
+
+  }
   return (
     <div className='flex justify-start items-center flex-col h-screen'>Login
       <div className="relative w-full h-full">
@@ -16,6 +21,29 @@ const Login = () => {
           autoplay
           className='w-full h-full object-cove'
         />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} alt="logo" width="130px" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId='624702655163-tss75arpd77i31bsr4uaptjgat2ut8dl.apps.googleusercontent.com'
+              render={(renderProps) => (
+                <button
+                  type='button'
+                  className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className='mr-4'/> Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
