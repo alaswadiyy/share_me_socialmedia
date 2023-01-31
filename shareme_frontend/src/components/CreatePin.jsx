730f8ab54c2b75e31de7a5b7721163e887c4fbcd
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-01-31 14:54:15 +0100

    k

--- shareme_frontend/src/components/CreatePin.jsx
+++ shareme_frontend/src/components/CreatePin.jsx
@@ -5,10 +5,48 @@ import { useNavigate } from 'react-router-dom';
 
 import { client } from '../client';
 import Spinner from './Spinner';
+import { categories } from '../utils/data';
 
 const CreatePin = () => {
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [destination, setDestination] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [fields, setFields] = useState(true);
+  const [category, setCategory] = useState(null);
+  const [imageAssets, setImageAssets] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(null);
+
+  const navigate = useNavigate();
+
+
   return (
-    <div>CreatePin</div>
+    <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
+      { fields && (
+        <p className='text-red-500 mb-5 text-xl transition-all duration-150 ease-in'>Please fill in all the fields.</p>
+      )}
+      <div className='flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full'>
+        <div className='bg-secondaryColor p-3 flex flex-0.7 w-full '>
+          <div className="flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420">
+            {loading && <Spinner />}
+            {wrongImageType && <p>Wrong image type</p>}
+            {!imageAssets ? (
+              <label>
+                <div className='flex flex-col items-center justify-center h-full'>
+                  <div className="flex flex-col justify-center items-center">
+                    <p className='font-bold text-2xl'>
+                      <AiOutlineCloudUpload />
+                    </p>
+                  </div>
+                </div>
+              </label>
+            ) : (
+              <p>something false</p>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
