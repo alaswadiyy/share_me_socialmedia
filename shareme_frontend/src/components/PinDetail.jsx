4a2fefb98db140c7bcf0870debeae79cd2d249d4
Abdulmuiz Sharafadeen <abdulmuizsharafadeen@gmail.com>
2023-02-10 03:33:26 +0100

    new commit

--- shareme_frontend/src/components/PinDetail.jsx
+++ shareme_frontend/src/components/PinDetail.jsx
@@ -7,6 +7,7 @@ import { client, urlFor } from "../client"
 import MasonryLayout from './MasonryLayout';
 import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
 import Spinner from './Spinner';
+import { isHtmlElement } from 'react-router-dom/dist/dom';
 
 const PinDetail = ({ user }) => {
   const [pins, setPins] = useState(null);
@@ -15,7 +16,6 @@ const PinDetail = ({ user }) => {
   const [addingComment, setAddingComment] = useState(false);
   const { pinId } = useParams();
 
-  if(!pinDetail) return <Spinner message="Loading pin...." />
     const fetchPinDetails = () => {
       let query = pinDetailQuery(pinId);
 
@@ -25,13 +25,75 @@ const PinDetail = ({ user }) => {
             setPinDetail(data[0])
 
             if(data[0]) {
-              query = pinDetailMorePinQuery(pinId)
+              query = pinDetailMorePinQuery(data[0]);
+
+              client.fetch(query)
+                .then((res) => setPins(res));
             }
           })
       }
     }
+
+    useEffect(() => {
+      fetchPinDetails()
+    }, [pinId])
+    
+  if(!pinDetail) return <Spinner message="Loading pin...." />
+
+
   return (
-    <div>PinDetail</div>
+    <div className='flex xl-flex-row flex-col m-auto bg-white' styles={{ maxwidth: '1500px', borderRadius: '32px'}}>
+      <div className='flex justify-center items-center md:items-start flex-initial'>
+        <img
+         src={pinDetail?.image && urlFor(pinDetail.image).url()}
+         className="rounded-t-3xl rounded-b-lg" 
+         alt="user-post" 
+        />
+      </div>
+      <div className='w-full p-5 flex-1 xl:min-w-620'>
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2 items-center">
+            <a 
+              href={`${pinDetail.image?.asset?.url}dl=`}
+              download
+              onClick={(e) => e.stopPropagation()}
+              className="bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none"
+            >
+                <MdDownloadForOffline />
+            </a>
+          </div>
+          <a href={pinDetail.destination} target="_blank" rel='noreferrer'>
+            {pinDetail.destination}
+          </a>
+        </div>
+        <div>
+          <h1 className='text-4xl font-bold break-words mt-3'>
+            {pinDetail.title}
+          </h1>
+          <p className='mt-3'>{pinDetail.about}</p>
+        </div>
+        <link to={`user-profile/${pinDetail.postedBy?._id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg">
+            <img
+                className='w-8 h-8 rounded-full object-cover'
+                src={pinDetail.postedBy?.image}
+                alt="user-profile"
+            />
+            <p className='font-semibold capitalize'>{pinDetail.postedBy?.userName}</p>
+        </link>
+        <h2 className='mt-5 text-2xl'>Comments</h2>
+        <div className="max-h-370 overflow-y-auto">
+          {pinDetail?.comments?.map((comment) => {
+            <div className='flex gap-2 mt-5 items-center bg-white rounded-lg' key={i}>
+              <img
+               src={comment.postedBy.image} 
+               alt="user-profile"
+               className='h-10 w-10 rounded-full cursor-pointer' 
+              />
+            </div>
+          })}
+        </div>
+      </div>
+    </div>
   )
 }
 
